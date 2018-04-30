93588b5729d8897c651fc1559893ef86bcc6ab3f
Farzad <farzad@InfrastturesMBP.home>
2018-04-30 16:27:55 +0100

    some code

---
+++ testing.nUnitTesting/Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+namespace testing.nUnitTesting
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            var p = new { id = 45, Name = "Farzad" };
+
+            Assert.AreEqual(p.id, 45);
+
+
+        }
+    }
+}
